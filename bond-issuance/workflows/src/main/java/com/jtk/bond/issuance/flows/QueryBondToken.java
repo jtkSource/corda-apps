e104ad8b31af7fbe6f291a8d9aa43265df3f58b3
jtkSource <jubin.kuriakose@gmail.com>
2022-08-09 23:14:33 +0800

    Added API to get total bonds issued by termId

--- bond-issuance/workflows/src/main/java/com/jtk/bond/issuance/flows/QueryBondToken.java
+++ bond-issuance/workflows/src/main/java/com/jtk/bond/issuance/flows/QueryBondToken.java
@@ -3,12 +3,10 @@ package com.jtk.bond.issuance.flows;
 import co.paralleluniverse.fibers.Suspendable;
 import com.jtk.bond.issuance.flows.utils.CustomQuery;
 import com.jtk.bond.issuance.state.BondState;
-import com.jtk.bond.issuance.state.TermState;
 import com.r3.corda.lib.tokens.contracts.types.TokenPointer;
 import com.r3.corda.lib.tokens.contracts.types.TokenType;
 import com.r3.corda.lib.tokens.workflows.utilities.QueryUtilities;
 import net.corda.core.contracts.Amount;
-import net.corda.core.contracts.StateAndRef;
 import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.flows.FlowException;
 import net.corda.core.flows.FlowLogic;
@@ -17,7 +15,6 @@ import net.corda.core.flows.StartableByRPC;
 import net.corda.core.utilities.ProgressTracker;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class QueryBondToken {
 
