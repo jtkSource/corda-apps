07efb535b3eba081602739e57e8ca97a65214d90
jtkSource <jubin.kuriakose@gmail.com>
2022-08-12 16:33:55 +0800

    fix typo

--- cash-issuance/contracts/src/main/java/com/jtk/corda/cash/issuance/contract/CashStateContract.java
+++ cash-issuance/contracts/src/main/java/com/jtk/corda/cash/issuance/contract/CashStateContract.java
@@ -1,6 +1,6 @@
 package com.jtk.corda.cash.issuance.contract;
 
-import com.jtk.corda.cash.issuance.state.CashSate;
+import com.jtk.corda.cash.issuance.state.CashState;
 import com.r3.corda.lib.tokens.contracts.EvolvableTokenContract;
 import net.corda.core.contracts.Contract;
 import net.corda.core.transactions.LedgerTransaction;
@@ -19,7 +19,7 @@ public class CashStateContract extends EvolvableTokenContract implements Contrac
     @Override
     public void verify(@NotNull LedgerTransaction tx) {
         log.info("Verifying the CashStateContract...");
-        CashSate cashSate = (CashSate) tx.getOutput(0);
+        CashState cashSate = (CashState) tx.getOutput(0);
         if(!tx.getCommand(0).getSigners().contains(cashSate.getIssuer().getOwningKey())){
             throw new IllegalArgumentException("Issuer signature is required!");
         }
@@ -28,15 +28,15 @@ public class CashStateContract extends EvolvableTokenContract implements Contrac
 
     @Override
     public void additionalCreateChecks(@NotNull LedgerTransaction tx) {
-        CashSate cashSate = tx.outputsOfType(CashSate.class).get(0);
+        CashState cashState = tx.outputsOfType(CashState.class).get(0);
         requireThat(req->{
-            req.using("Currency code cannot be empty ", (cashSate.getCurrencyCode()!=null && cashSate.getCurrencyCode().length()>0));
+            req.using("Currency code cannot be empty ", (cashState.getCurrencyCode()!=null && cashState.getCurrencyCode().length()>0));
             try {
-                Currency currency = Currency.getInstance(cashSate.getCurrencyCode());
+                Currency currency = Currency.getInstance(cashState.getCurrencyCode());
             }catch (Exception e){
-                req.using("Currency code should be valid: " + cashSate.getCurrencyCode(), false);
+                req.using("Currency code should be valid: " + cashState.getCurrencyCode(), false);
             }
-            req.using("Currency USD pair rate cannot be zero",cashSate.getUsdPairRate()!=0.0);
+            req.using("Currency USD pair rate cannot be zero", cashState.getUsdPairRate()!=0.0);
 
             return null;
         });
@@ -45,8 +45,8 @@ public class CashStateContract extends EvolvableTokenContract implements Contrac
 
     @Override
     public void additionalUpdateChecks(@NotNull LedgerTransaction tx) {
-        CashSate inputState = tx.inputsOfType(CashSate.class).get(0);
-        CashSate outputState = tx.outputsOfType(CashSate.class).get(0);
+        CashState inputState = tx.inputsOfType(CashState.class).get(0);
+        CashState outputState = tx.outputsOfType(CashState.class).get(0);
         requireThat(req->{
             req.using("Currency code cannot be changed", inputState.getCurrencyCode() == outputState.getCurrencyCode());
             req.using("Issuer cannot be changed", inputState.getIssuer() == outputState.getIssuer());
